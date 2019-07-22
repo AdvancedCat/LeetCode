13ceb5a556c49b0ff169ca8dddded5a1d2b80b0f
hongxin <hongxin@163.com>
2019-07-22 22:50:00 +0800

    feat: two sum problem

---
+++ problems/1.two-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+
+    const dic = {}
+
+    nums.forEach((num, idx)=>{
+        if(!dic['' + num]){
+            dic['' + num] = idx
+        }
+    })
+
+    for(let i=0; i<nums.length; i++){
+        let v = '' + (target - nums[i])
+        if(dic[v]){
+            return [i, dic[v]]
+        }
+    }
+
+    return []
+};
+
+export default twoSum
---
+++ problems/__tests__/1.spec.js
@@ -0,0 +1,21 @@
+import twoSum from '../1.two-sum'
+
+describe('two sum', ()=>{
+    it('case 1', ()=>{
+        const result = twoSum(
+            [2, 7, 11, 15],
+            9
+        )
+
+        expect(result).toEqual([0,1])
+    })
+
+    it('case 2', ()=>{
+        const result = twoSum(
+            [3,8,12,7],
+            10
+        )
+
+        expect(result).toEqual([0,3])
+    })
+})
