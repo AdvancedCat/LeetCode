0eca8bd23893394c7584085bcda3d5a70c7c44a7
HongXin <hongxin@jd.com>
2019-07-23 09:26:20 +0800

    style: format

--- problems/1.two-sum.js
+++ problems/1.two-sum.js
@@ -4,23 +4,22 @@
  * @return {number[]}
  */
 function twoSum(nums, target) {
-
     const dic = {}
 
-    nums.forEach((num, idx)=>{
-        if(!dic['' + num]){
+    nums.forEach((num, idx) => {
+        if (!dic['' + num]) {
             dic['' + num] = idx
         }
     })
 
-    for(let i=0; i<nums.length; i++){
+    for (let i = 0; i < nums.length; i++) {
         let v = '' + (target - nums[i])
-        if(dic[v]){
+        if (dic[v]) {
             return [i, dic[v]]
         }
     }
 
     return []
-};
+}
 
 export default twoSum
--- problems/__tests__/1.spec.js
+++ problems/__tests__/1.spec.js
@@ -1,21 +1,15 @@
 import twoSum from '../1.two-sum'
 
-describe('two sum', ()=>{
-    it('case 1', ()=>{
-        const result = twoSum(
-            [2, 7, 11, 15],
-            9
-        )
+describe('two sum', () => {
+    it('case 1', () => {
+        const result = twoSum([2, 7, 11, 15], 9)
 
-        expect(result).toEqual([0,1])
+        expect(result).toEqual([0, 1])
     })
 
-    it('case 2', ()=>{
-        const result = twoSum(
-            [3,8,12,7],
-            10
-        )
+    it('case 2', () => {
+        const result = twoSum([3, 8, 12, 7], 10)
 
-        expect(result).toEqual([0,3])
+        expect(result).toEqual([0, 3])
     })
 })
