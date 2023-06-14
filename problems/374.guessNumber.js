0049a72865134c0662797558e35bbc35c892cea6
advancedcat <wshx1938@163.com>
2023-06-14 22:59:05 +0800

    feat: problem 374

--- problems/374.guessNumber.js
+++ problems/374.guessNumber.js
@@ -1,33 +1,15 @@
-/**
- * Forward declaration of guess API.
- * @param {number} num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * var guess = function(num) {}
- */
-
-/**
- * @param {number} n
- * @return {number}
- */
 var guessNumber = function(n) {
-    return guessBetween(1, n)
-}
-
-function guessBetween(a, b) {
-    const mid = Math.floor(a + b / 2)
-    const result = guess(mid)
-
-    if (result === -1) {
-        return guessBetween(a, mid - 1)
+    let left = 1,
+        right = n
+    while (left < right) {
+        // 循环直至区间左右端点相同
+        const mid = Math.floor(left + (right - left) / 2)
+        if (guess(mid) <= 0) {
+            right = mid // 答案在区间 [left, mid] 中
+        } else {
+            left = mid + 1 // 答案在区间 [mid+1, right] 中
+        }
     }
-
-    if (result === 1) {
-        return guessBetween(mid, b)
-    }
-
-    if (result === 0) {
-        return mid
-    }
+    // 此时有 left == right，区间缩为一个点，即为答案
+    return left
 }
