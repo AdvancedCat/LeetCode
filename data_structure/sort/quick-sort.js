376033c2f5f5ea70e44188a2f6e384097d6adaf2
advancedcat <wshx1938@163.com>
2023-06-21 17:48:30 +0800

    chore: some practices

---
+++ data_structure/sort/quick-sort.js
@@ -0,0 +1,29 @@
+const quickSort = array => {
+    const length = array.length
+    if (length <= 1) {
+        return array
+    }
+
+    const midIndex = Math.floor(length / 2)
+    const midValue = array.splice(midIndex, 1)[0]
+    let leftArray = []
+    let rightArray = []
+    let index = 0
+
+    while (index < length - 1) {
+        const curValue = array[index]
+
+        if (curValue <= midValue) {
+            leftArray.push(curValue)
+        } else {
+            rightArray.push(curValue)
+        }
+
+        index++
+    }
+
+    return quickSort(leftArray).concat([midValue], quickSort(rightArray))
+}
+
+const list = [3, 2, 3]
+console.log(quickSort(list))
