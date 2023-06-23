894f8ed2d2c8a0bdf683b8cfeb93b355e8fcbc56
advancedcat <wshx1938@163.com>
2023-06-23 15:55:56 +0800

    chore: some problems

---
+++ problems/32.longestValidParentheses.js
@@ -0,0 +1,25 @@
+// @see: https://leetcode.cn/problems/longest-valid-parentheses/
+const longestValidParentheses = s => {
+    let maxLen = 0
+    const stack = []
+    stack.push(-1)
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i]
+        if (c == '(') {
+            // 左括号的索引，入栈
+            stack.push(i)
+        } else {
+            // 遍历到右括号
+            stack.pop() // 栈顶的左括号被匹配，出栈
+            if (stack.length) {
+                // 栈未空
+                const curMaxLen = i - stack[stack.length - 1] // 计算有效连续长度
+                maxLen = Math.max(maxLen, curMaxLen) // 挑战最大值
+            } else {
+                // 栈空了
+                stack.push(i) // 入栈充当参照
+            }
+        }
+    }
+    return maxLen
+}
